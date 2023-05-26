0929271f1e5d5bcc308737e6eb744d233ec11339
Shashwat5522 <srmehta5522@gmail.com>
2023-05-26 18:28:44 +0530

    teacher panel added

--- controllers/postquizcontroller.go
+++ controllers/postquizcontroller.go
@@ -1,16 +1,50 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+
+	// "quizz-application/dbconnection"
+	"quizz-application/dbconnection"
+	"quizz-application/models"
 
 	"github.com/gin-gonic/gin"
 )
 
 func PostQuizController(c *gin.Context) {
+	quizname :=c.Query("quizname")
+	fmt.Println(quizname)
 	fmt.Println("post request called")
-	var question map[string]interface{}
+	// fmt.Println(c.Request.Body)
+	var questions []models.Questions
 
-	c.ShouldBind(&question)
-	fmt.Println(question)
+	body, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		c.AbortWithError(400, err)
+		return
+	}
+	err = json.Unmarshal(body, &questions)
+	fmt.Println(len(questions))
+	if err != nil {
+		c.AbortWithError(400, err)
+		return
+
+	}
+	fmt.Println(quizname)
+	fmt.Println(questions)
+	q := models.Quiz{
+		QuizName: quizname,
+	}
+
+	dbconnection.DB.Create(&q)
+
+	for i := 0; i < len(questions); i++ {
+		//
+		questions[i].QuizId = q.ID
+		dbconnection.DB.Create(&questions[i])
+	}
+
+	fmt.Println("data added!!!")
 
 }
